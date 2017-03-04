b57c5d6143f67fa24a2967ec0466ee1100efc9e8
Alexander Zhidkov <Alexander Zhidkov>
2017-03-04 15:19:16 +0800

    AngularFire

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { MaterialModule } from '@angular/material';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBo41ERx8-sWyLWx56WvFc-RH6-nCvpFTI',
+  authDomain: 'partways-dev.firebaseapp.com',
+  databaseURL: 'https://partways-dev.firebaseio.com',
+  storageBucket: 'partways-dev.appspot.com',
+  messagingSenderId: '1727367456'
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
